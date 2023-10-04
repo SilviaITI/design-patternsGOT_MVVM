21631679027708f31388869efd342541b68dcdd4
silviaITI <silvia.casanovamartinez@gmail.com>
2023-10-04 17:59:27 +0200

    add files

---
+++ Design-patternsGOT_MVVM/Home/HomeCell/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  Design-patternsGOT_MVVM
+//
+//  Created by Silvia Casanova Martinez on 4/10/23.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Design-patternsGOT_MVVM/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Design-patternsGOT_MVVM
+//
+//  Created by Silvia Casanova Martinez on 4/10/23.
+//
+
+import Foundation
